abd69100dbe59d801ee3eaa70e7b08eb784a50fb
stewart <stewart_robertson@sky.com>
2016-10-19 00:43:16 +0100

    wip

--- FsEntries/2016-10-15-D3-Network-Diagrams-in-F#.fsx
+++ FsEntries/2016-10-15-D3-Network-Diagrams-in-F#.fsx
@@ -3,17 +3,22 @@
 #load "../packages/FsLab/FsLab.fsx"
 open Blog
 let frontmatter ={
-    Title = "D3 Network (Force Layout) Diagrams in F#"
+    Title = "D3 Network Graphs in F#"
     Layout = "post"
 }
 frontmatter
 (*** include-value:frontmatter ***)
 
 (**
-Some Markdown
--------------
+## Sub header
+### Sub Sub header
+#### Sub Sub Sub header
+
 If you prefer to use F# Formatting tools via the command line, you can use the `FSharp.Formatting.CommandTool` package, which includes an executable `fsformatting.exe` 
 that gives you access to the most important functionality via a simple command line interface. This might be a good idea if you prefer to run F# Formatting as a 
 separate process, e.g. for resource management reasons.
 *)
+
+let x = 1
+let y = x + 3;
 
