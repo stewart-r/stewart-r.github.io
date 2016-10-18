d33d6c676d2ce1fb1cf699af9b9a8307eb1ca660
stewart <stewart_robertson@sky.com>
2016-10-19 00:12:13 +0100

    wip

--- FsEntries/2016-10-15-D3-Network-Diagrams-in-F#.fsx
+++ FsEntries/2016-10-15-D3-Network-Diagrams-in-F#.fsx
@@ -12,7 +12,8 @@ frontmatter
 (**
 Some Markdown
 -------------
-
-
+If you prefer to use F# Formatting tools via the command line, you can use the `FSharp.Formatting.CommandTool` package, which includes an executable `fsformatting.exe` 
+that gives you access to the most important functionality via a simple command line interface. This might be a good idea if you prefer to run F# Formatting as a 
+separate process, e.g. for resource management reasons.
 *)
 
